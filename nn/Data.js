5980744fac697b03a58b19058aab8e9bb4cf2802
songshi_zhang <songshi_zhang@apple.com>
2023-07-04 01:39:32 +0800

    update

--- nn/Data.js
+++ nn/Data.js
@@ -1,33 +1,66 @@
 import config from "./config.js";
 
 const dataSize = config.data_size;
-const data = [];
 
-for (let i = 0; i < dataSize; i++) {
-  const a = Math.random() * 100,
-    b = Math.random() * 100,
-    c = Math.random() * 100;
-  data.push([
-    a,
-    b,
-    c,
-    a + b + 3 * c + 0.4 * a * b + 0.3 * b * c + 0.5 * a * c + 0.001 * a * b * c,
-    // a + b + 3 * c + 0.4,
-  ]);
+function polynomial() {
+  const data = [];
+
+  for (let i = 0; i < dataSize; i++) {
+    const a = Math.random() * 100,
+      b = Math.random() * 100,
+      c = Math.random() * 100;
+    data.push([
+      a,
+      b,
+      c,
+      a +
+        b +
+        3 * c +
+        0.4 * a * b +
+        0.3 * b * c +
+        0.5 * a * c +
+        0.001 * a * b * c,
+      // a + b + 3 * c + 0.4,
+    ]);
+  }
+  return data;
 }
-// for (let i = 0; i < dataSize; i++) {
-//   const a = Math.random() * 100,
-//     b = Math.random() * 100;
-//   data.push([a, b, a + b]);
-// }
 
-// for (let i = 0; i < dataSize; i++) {
-//   const a = Math.floor(Math.random() * 100);
-//   const b = Math.floor(Math.random() * 100);
-//   const c = Math.floor(Math.random() * 100);
-//   data.push([a, b, c, Math.findMaxIndex([a, b, c])]);
-// }
+function linear() {
+  const data = [];
+  for (let i = 0; i < dataSize; i++) {
+    const a = Math.random() * 100,
+      b = Math.random() * 100;
+    data.push([a, b, a + 1]);
+  }
+  return data;
+}
 
+function classification() {
+  const data = [];
+
+  for (let i = 0; i < dataSize; i++) {
+    const a = Math.floor(Math.random() * 100);
+    const b = Math.floor(Math.random() * 100);
+    const c = Math.floor(Math.random() * 100);
+    data.push([a, b, c, Math.findMaxIndex([a, b, c])]);
+  }
+  return data;
+}
+
+function classification2() {
+  const data = [];
+
+  for (let i = 0; i < dataSize; i++) {
+    const a = Math.floor(Math.random() * 100);
+    const b = Math.floor(Math.random() * 100);
+    const c = Math.floor(Math.random() * 100);
+    data.push([a, b, c, Math.floor((a + b + c) / 100)]);
+  }
+  return data;
+}
+
+const data = classification2();
 const trainingDataSize = config.training_size(config.data_size);
 const trainingData = data.slice(0, trainingDataSize);
 const testData = data.slice(trainingDataSize);
--- nn/config.js
+++ nn/config.js
@@ -25,14 +25,16 @@ const config = {
     return Math.floor(size * 0.7);
   },
 
-  loss_function: LossFunction.MSE,
+  loss_function: LossFunction.CE,
 
   default_activation: Activation.leakyRelu,
-  default_output_activation: Activation.leakyRelu,
+  default_output_activation: Activation.softmax,
+
   data_size: 1000,
   epoch: 50000,
   batch_size: 50,
-  shape: [5, 3, 1],
+  shape: [5, 3, 3],
+
   features: [
     (data) => data[0],
     (data) => data[1],
@@ -48,9 +50,9 @@ const config = {
     (data) => (data[1] * data[2]) / 100,
     // (data) => (data[0] * data[1] * data[2]) / 10000,
   ],
-  fn_judge: function_judge_regression,
+  fn_judge: function_judge_classification,
   fn_true_value: (data) => data[true_column_index],
-  learning_rate: 0.0000003,
+  learning_rate: 0.000003,
   updateLearningRate: function (epoch, round) {
     // if (epoch % 100 == 0 && round == 0) this.learning_rate *= 0.99999;
   },
